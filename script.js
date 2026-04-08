e5d240704e65b8e829aedaf649bae71cecc15cbe
raohimansh <rao.sahab.pvt.ltd@gmail.com>
2026-04-08 10:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function toggleMenu() {
   let menu = document.getElementById("mobileMenu");
-
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
+  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
 }
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    document.getElementById("mobileMenu").style.display = "none";
+  });
+});

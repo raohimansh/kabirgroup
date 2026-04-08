12de1706d8a416df285116b2514c9642f6ce31f9
raohimansh <rao.sahab.pvt.ltd@gmail.com>
2026-04-08 10:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function toggleMenu() {
-  let menu = document.getElementById("mobileMenu");
-  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+function toggleMenu(){
+  let m = document.getElementById("mobileMenu");
+  m.style.display = (m.style.display==="flex") ? "none":"flex";
 }
 
-document.querySelectorAll(".mobile-menu a").forEach(link => {
-  link.addEventListener("click", () => {
-    document.getElementById("mobileMenu").style.display = "none";
-  });
-});
+/* Real Search */
+function searchSite(){
+  let query = document.getElementById("searchInput").value.toLowerCase();
+
+  if(query.includes("meter")) window.location.href="services.html";
+  else if(query.includes("electrical")) window.location.href="services.html";
+  else if(query.includes("contact")) window.location.href="contact.html";
+  else alert("No result found");
+}
